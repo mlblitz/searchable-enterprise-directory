05359d392b888c7451004dcd57ab5557fe7e2ce4
NoahMerone <nmerone@travelers.com>
2023-02-09 15:47:57 -0500

    css changes

--- react-app/src/components/Results.js
+++ react-app/src/components/Results.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Results.css';
 
+
 function Results({ employees, currentUser }) {
   return (
     <div className="results-container">
@@ -11,11 +12,11 @@ function Results({ employees, currentUser }) {
             <h3>{employee.first_name} {employee.last_name}</h3>
           </div>
           <div className="employee-details">
-            <p>Id: {employee._id}</p>
+            <p>Id: {employee.emp_id}</p>
             <p>Phone Number: {employee.phone_number}</p>
             <p>Job Role: {employee.job_role}</p>
             <p>Work Location: {employee.work_location}</p>
-            {currentUser !== -1 && (
+            {(currentUser === employee.emp_id || currentUser === employee.manager) && (
               <p>Salary: {employee.salary}</p>
             )}
           </div>
